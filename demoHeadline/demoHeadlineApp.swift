7886f5949b44ae4084b8974c50d6e478886b8a94
Leksovich <imagemakerspa@gmail.com>
2024-06-02 22:54:26 +0300

    Initial Commit

---
+++ demoHeadline/demoHeadlineApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoHeadlineApp.swift
+//  demoHeadline
+//
+//  Created by Pavel Semenchenko on 02.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct demoHeadlineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
