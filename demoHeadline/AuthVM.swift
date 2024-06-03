c50521b82dc23b3b9971a297e8f25a837622090f
Leksovich <imagemakerspa@gmail.com>
2024-06-03 22:18:14 +0300

    try add auth

---
+++ demoHeadline/AuthVM.swift
@@ -0,0 +1,18 @@
+//
+//  AuthVM.swift
+//  demoHeadline
+//
+//  Created by Pavel Semenchenko on 02.06.2024.
+//
+
+import SwiftUI
+
+struct AuthVM: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthVM()
+}
---
+++ demoHeadline/SignInView.swift
@@ -0,0 +1,18 @@
+//
+//  SignInView.swift
+//  demoHeadline
+//
+//  Created by Pavel Semenchenko on 02.06.2024.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignInView()
+}
