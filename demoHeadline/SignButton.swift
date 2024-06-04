45cbf0bd1b443a9e66d13786c90a2ea2ad65161d
Leksovich <imagemakerspa@gmail.com>
2024-06-04 22:02:44 +0300

    created button

---
+++ demoHeadline/SignButton.swift
@@ -0,0 +1,18 @@
+//
+//  SignButton.swift
+//  demoHeadline
+//
+//  Created by Pavel Semenchenko on 04.06.2024.
+//
+
+import SwiftUI
+
+struct SignButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignButton()
+}
