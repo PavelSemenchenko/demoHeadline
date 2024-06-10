44e6d39344b62b6c0a279bcffb202b558d8bd7f5
Leksovich <imagemakerspa@gmail.com>
2024-06-10 19:47:56 +0300

    begin user profile screen

---
+++ demoHeadline/Screens/ProfileSetupScreen.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileSetupScreen.swift
+//  demoHeadline
+//
+//  Created by Pavel Semenchenko on 10.06.2024.
+//
+
+import SwiftUI
+
+struct ProfileSetupScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileSetupScreen()
+}
