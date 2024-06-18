79dc0fd8144e4479a1e243ed625b92527e1e9a53
Leksovich <imagemakerspa@gmail.com>
2024-06-18 20:08:02 +0300

    added tabbar

---
+++ demoHeadline/TabBar.swift
@@ -0,0 +1,34 @@
+//
+//  TabBar.swift
+//  demoHeadline
+//
+//  Created by Pavel Semenchenko on 18.06.2024.
+//
+
+import SwiftUI
+
+enum TabBarId: Int, Hashable {
+    case home = 0
+    case profile = 1
+}
+
+struct TabBar: View {
+    @EnvironmentObject private var navigationVM: NavigationRouter
+    @State var currentTab = TabBarId.home
+    
+    var body: some View {
+        TabView(selection: $currentTab){
+            HomeScreen()
+                .tabItem { Text("Home") }
+                .tag(0)
+            ProfileSetupScreen()
+                .tabItem { Text("Profile") }
+                .tag(1)
+        }.navigationBarBackButtonHidden(true)
+    }
+}
+
+#Preview {
+    TabBar().environmentObject(NavigationRouter())
+        .environmentObject(AuthVM())
+}
