653b5d1c3afa99cffc935709451bc17f98012f20
Leksovich <imagemakerspa@gmail.com>
2024-07-07 22:00:50 +0300

    try add web view

---
+++ demoHeadline/Screens/WebSourceScreen.swift
@@ -0,0 +1,18 @@
+//
+//  WebSourceScreen.swift
+//  demoHeadline
+//
+//  Created by Pavel Semenchenko on 07.07.2024.
+//
+
+import SwiftUI
+
+struct WebSourceScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WebSourceScreen()
+}
